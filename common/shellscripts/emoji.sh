74b7a5eceb5797fa516642658c4cc3bc8e2bb5df
Luiz Ferraz <luiz@lferraz.com>
2022-08-16 05:12:32 -0300

    Include shellscripts on NixOS

--- common/shellscripts/emoji.sh
+++ common/shellscripts/emoji.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #=============================================================================
 # Inspired by @TheCodeTherapy - https://mgz.me
@@ -10,11 +10,11 @@
 # Shortcut: Super + . (or any other you might want)
 #=============================================================================
 
-EMOJIS="/home/${USER}/ZShutils/emoji/emoji.txt"
-THEME="/home/${USER}/ZShutils/rofi/sp.theme.rasi"
+EMOJIS="/home/${USER}/ZShutils/common/emoji/emoji.txt"
+THEME="/home/${USER}/ZShutils/common/rofi/sp.theme.rasi"
 chosen=$(cut -d ';' -f1 ${EMOJIS} | rofi -dmenu -theme ${THEME} | sed "s/ .*//")
 [ -z "$chosen" ] && exit
 
 echo "$chosen" | tr -d '\n' | xclip -selection clipboard
-notify-send -a "Emoji Picker" "'$chosen' copied to clipboard." --expire-time=2100 &
+# notify-send -a "Emoji Picker" "'$chosen' copied to clipboard." --expire-time=2100 &
 
--- nixos/home/dev-tools.nix
+++ nixos/home/dev-tools.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = with pkgs; [
     rustup
@@ -14,5 +14,16 @@
   home.sessionPath = [
     "$HOME/.cargo/bin"
     "$HOME/go/bin"
+    "$HOME/.local/bin"
   ];
+
+  home.file = lib.mapAttrs'
+    (name: _: {
+      name = ".local/bin/${name}";
+      value = {
+        source = ../../common/shellscripts/${name};
+        executable = true;
+      };
+    })
+    (lib.filterAttrs (_: typ: typ == "regular") (builtins.readDir ../../common/shellscripts));
 }
