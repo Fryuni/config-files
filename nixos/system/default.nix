c26b85a8907efffc624714429375d587a38cf53c
Luiz Ferraz <luiz@lferraz.com>
2022-08-27 05:37:03 -0300

    Configure Thunar plugins

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -40,6 +40,12 @@
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+    thunar-volman
+  ];
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
