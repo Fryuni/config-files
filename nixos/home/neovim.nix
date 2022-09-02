a488da725c93b7ce32ca8c5f03bd2a138ea32ff1
Luiz Ferraz <luiz@lferraz.com>
2022-09-02 15:47:17 -0300

    Add more neovim configuration

--- nixos/home/neovim.nix
+++ nixos/home/neovim.nix
@@ -1,14 +1,29 @@
 { pkgs, ... }:
 {
+  # TODO: Follow this guide to the end: https://www.youtube.com/watch?v=rUvjkBuKua4
+
+  xdg.configFile = {
+    "nvim/parser/nix.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-nix}/parser";
+  };
+
   programs.neovim = {
     enable = true;
 
-    coc.enable = true;
-    coc.settings = {
-      languageserver = {
-        nix = {
-          command = "rnix-lsp";
-          filetypes = [ "nix" ];
+    plugins = with pkgs.vimPlugins; [
+      nvim-tree-lua
+      nvim-treesitter
+
+      vim-nix
+    ];
+
+    coc = {
+      enable = true;
+      settings = {
+        languageserver = {
+          nix = {
+            command = "rnix-lsp";
+            filetypes = [ "nix" ];
+          };
         };
       };
     };
