978b0961f9a792587f98b76a9693987b0e8be309
Luiz Ferraz <luiz@lferraz.com>
2022-09-22 23:19:49 -0300

    Use nixos-unstable branch of nixpkgs instead of master

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "Fryuni's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
