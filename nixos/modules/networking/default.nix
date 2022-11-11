92bf794d8b39348c339fb528e38cf1722f76ad31
Luiz Ferraz <luiz@lferraz.com>
2022-11-10 21:24:27 -0300

    chore: Format Nix files

--- nixos/modules/networking/default.nix
+++ nixos/modules/networking/default.nix
@@ -4,7 +4,7 @@ with lib;
   # Enable networking
   networking.networkmanager.enable = true;
 
-networking.enableIPv6 = false;
+  networking.enableIPv6 = false;
 
   services.openvpn.servers =
     let
@@ -14,27 +14,31 @@ networking.enableIPv6 = false;
         let
           modeFiles = builtins.attrNames (builtins.readDir "${./nordvpn}/${mode}");
         in
-        builtins.map (file: 
-          let
-            nameParts = strings.splitString "." file;
-          in
-          { 
-          provider = "nord";
-          protocol = builtins.elemAt nameParts 3;
-          name = builtins.elemAt nameParts 0;
-          configFile = "${./nordvpn}/${mode}/${file}";
-          }) modeFiles;
+        builtins.map
+          (file:
+            let
+              nameParts = strings.splitString "." file;
+            in
+            {
+              provider = "nord";
+              protocol = builtins.elemAt nameParts 3;
+              name = builtins.elemAt nameParts 0;
+              configFile = "${./nordvpn}/${mode}/${file}";
+            })
+          modeFiles;
 
       nordServers = lists.flatten (builtins.map nordModeToFiles nordModeNames);
 
-      configs = builtins.map (server: {
-        name = "${server.provider}.${server.protocol}.${server.name}";
-        value = {
-          # authUserPass.username = "luiz@lferraz.com";
-          autoStart = false;
-          config = "config ${server.configFile}";
+      configs = builtins.map
+        (server: {
+          name = "${server.provider}.${server.protocol}.${server.name}";
+          value = {
+            # authUserPass.username = "luiz@lferraz.com";
+            autoStart = false;
+            config = "config ${server.configFile}";
           };
-      }) nordServers;
+        })
+        nordServers;
     in
     builtins.listToAttrs configs;
 }
