9e67e761c3ba4527bd0ef3465c0a4147389e3a6d
Luiz Ferraz <luiz@lferraz.com>
2022-12-11 13:27:04 -0300

    feat: Register flake iself on system registry

--- nixos/registries.nix
+++ nixos/registries.nix
@@ -22,6 +22,9 @@ in {
   ];
 
   nix.registry = {
+    # Register this flake itself on the registry
+    me.flake = inputs.self;
+
     nixpkgs.flake = inputs.nixpkgs;
     nixpkgs-stable.flake = inputs.nixpkgs-stable;
     nixpkgs-master.flake = inputs.nixpkgs-master;
