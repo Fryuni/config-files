010a829c1539481aaae300bb9d6c82b6e501dc6d
Luiz Ferraz <luiz@lferraz.com>
2022-12-11 13:24:49 -0300

    chore(flake): Expose maintenance commands as both devShell commands and flake apps

--- flake.nix
+++ flake.nix
@@ -17,22 +17,24 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    flake-utils,
-    devshell,
-    ...
-  } @ attrs: let
-    pkgFun = system: let
-      pkgConfig = {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in
-      import nixpkgs (pkgConfig
-        // {
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , flake-utils
+    , devshell
+    , ...
+    } @ attrs:
+    let
+      pkgFun = system:
+        let
+          pkgConfig = {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        import nixpkgs (pkgConfig
+          // {
           overlays = [
             (_: _: {
               master = import attrs.nixpkgs-master pkgConfig;
@@ -44,8 +46,8 @@
           ];
         });
 
-    pkgs = pkgFun builtins.currentSystem;
-  in
+      pkgs = pkgFun (builtins.currentSystem or flake-utils.lib.system.x86_64-linux);
+    in
     {
       nixosConfigurations.notebook = nixpkgs.lib.nixosSystem {
         system = builtins.currentSystem;
@@ -67,17 +69,175 @@
         ];
       };
     }
-    // flake-utils.lib.eachDefaultSystem (system: let
+    // flake-utils.lib.eachDefaultSystem (system:
+    let
       pkgs = pkgFun system;
-    in {
+      commands = [
+        # Setup
+        {
+          name = "activate/notebook";
+          category = "setup";
+          help = "Install home-manager and apply home configuration";
+          command = ''
+            export HOME_MANAGER_BACKUP_EXT=old
+            ${self.outputs.homeConfigurations.notebook.activationPackage}/activate
+          '';
+        }
+
+        # --- Home Environment ---
+        {
+          name = "ls-pkg";
+          category = "Home";
+          help = "List all packages installed in home-manager-path";
+          command = ''
+            home-manager packages
+          '';
+        }
+        {
+          name = "ls-gen";
+          category = "Home";
+          help = "List all home environment generations";
+          command = ''
+            home-manager generations
+          '';
+        }
+        {
+          name = "diff";
+          category = "Home";
+          help = "Compute the package difference that will be applied to home-manager on a switch";
+          command = ''
+            ${self.outputs.apps.${system}.build.program}
+            ${pkgs.nvd}/bin/nvd diff /nix/var/nix/profiles/per-user/$USER/home-manager ./result
+          '';
+        }
+        {
+          name = "build";
+          category = "Home";
+          help = "Build home-manager configuration without applying it";
+          command = ''
+            home-manager build --flake '.#notebook' -b bck --impure $@
+          '';
+        }
+        {
+          name = "switch";
+          category = "Home";
+          help = "Switch home-manager to apply home config changes";
+          command = ''
+            home-manager switch --flake '.#notebook' -b bck --impure $@
+          '';
+        }
+        {
+          name = "update";
+          category = "Home";
+          help = "Update the flake lock file only";
+          command = ''
+            nix flake update $@
+          '';
+        }
+
+        # NixOS
+        {
+          name = "os-switch";
+          category = "NixOS";
+          help = "Apply NixOS configuration and configure it as the default profile";
+          command = ''
+            sudo nixos-rebuild switch --flake '.#notebook' --impure $@
+          '';
+        }
+        {
+          name = "os-test";
+          category = "NixOS";
+          help = "Apply NixOS configuration but don't set it on any boot entry";
+          command = ''
+            sudo nixos-rebuild test --flake '.#notebook' --impure $@
+          '';
+        }
+        {
+          name = "os-diff";
+          category = "NixOS";
+          help = "Compute the package difference that will be applied to the system on a switch";
+          command = ''
+            ${self.outputs.apps.${system}.os-build.program}
+            ${pkgs.nvd}/bin/nvd diff /run/current-system ./result
+          '';
+        }
+        {
+          name = "os-build";
+          category = "NixOS";
+          help = "Build NixOS configuration without applying";
+          command = ''
+            nixos-rebuild build --flake '.#notebook' --impure $@
+          '';
+        }
+        {
+          name = "os-boot";
+          category = "NixOS";
+          help = "Apply NixOS configuration as the default boot profile, but don't load it immediately";
+          command = ''
+            sudo nixos-rebuild boot --flake '.#notebook' --impure $@
+          '';
+        }
+        {
+          name = "os-purge";
+          category = "NixOS";
+          help = "Drop a system profile and purges it from the bootloader";
+          command = ''
+            sudo rm -vrf /nix/var/nix/profiles/system-profiles/$1 /nix/var/nix/profiles/system-profiles/$1-*-link
+            sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
+          '';
+        }
+
+        # Utility
+        {
+          name = "clear";
+          category = "Utility";
+          help = "Clear build result";
+          command = ''
+            rm -rf ./result
+          '';
+        }
+        {
+          name = "fmt";
+          category = "Utility";
+          help = "Format nix files";
+          command = ''
+            ${pkgs.statix}/bin/statix fix
+            nix fmt $@
+          '';
+        }
+        {
+          name = "gc";
+          category = "Utility";
+          help = "Garbage collection";
+          command = ''
+            sudo nix-collect-garbage $@
+          '';
+        }
+        {
+          name = "gc-all-gen";
+          category = "Utility";
+          help = ''Delete old generations and garbage collection'';
+          command = ''
+            sudo nix-collect-garbage -d $@
+          '';
+        }
+      ];
+    in
+    {
       formatter = pkgs.alejandra;
 
-      apps = {
-        "activate/notebook" = {
-          type = "app";
-          program = "${self.outputs.homeConfigurations.notebook.activationPackage}/activate";
-        };
-      };
+      apps =
+        let
+          commandToApp = cmd: {
+            inherit (cmd) name;
+            value = {
+              type = "app";
+              program = builtins.toString (pkgs.writeShellScript cmd.name cmd.command);
+            };
+          };
+          appList = builtins.map commandToApp commands;
+        in
+        builtins.listToAttrs appList;
 
       devShells.default = pkgs.devshell.mkShell {
         devshell.motd = ''
@@ -85,156 +245,11 @@
           $(type -p menu &>/dev/null && menu)
         '';
 
-        commands = [
-          # Setup
-          {
-            name = "install-notebook";
-            category = "setup";
-            help = "Install home-manager and apply home configuration";
-            command = ''
-              export HOME_MANAGER_BACKUP_EXT=old
-              nix run '.#activate/notebook'
-            '';
-          }
-
-          # --- Home Environment ---
-          {
-            name = "ls-pkg";
-            category = "Home";
-            help = "List all packages installed in home-manager-path";
-            command = ''
-              home-manager packages
-            '';
-          }
-          {
-            name = "ls-gen";
-            category = "Home";
-            help = "List all home environment generations";
-            command = ''
-              home-manager generations
-            '';
-          }
-          {
-            name = "diff";
-            category = "Home";
-            help = "Compute the package difference that will be applied to home-manager on a switch";
-            command = ''
-              build
-              ${pkgs.nvd}/bin/nvd diff /nix/var/nix/profiles/per-user/$USER/home-manager ./result
-            '';
-          }
-          {
-            name = "build";
-            category = "Home";
-            help = "Build home-manager configuration without applying it";
-            command = ''
-              home-manager build --flake '.#notebook' -b bck --impure $@
-            '';
-          }
-          {
-            name = "switch";
-            category = "Home";
-            help = "Switch home-manager to apply home config changes";
-            command = ''
-              home-manager switch --flake '.#notebook' -b bck --impure $@
-            '';
-          }
-          {
-            name = "update";
-            category = "Home";
-            help = "Update the flake lock file only";
-            command = ''
-              nix flake update $@
-            '';
-          }
-
-          # NixOS
-          {
-            name = "os-switch";
-            category = "NixOS";
-            help = "Apply NixOS configuration and configure it as the default profile";
-            command = ''
-              sudo nixos-rebuild switch --flake '.#notebook' --impure $@
-            '';
-          }
-          {
-            name = "os-test";
-            category = "NixOS";
-            help = "Apply NixOS configuration but don't set it on any boot entry";
-            command = ''
-              sudo nixos-rebuild test --flake '.#notebook' --impure $@
-            '';
-          }
-          {
-            name = "os-diff";
-            category = "NixOS";
-            help = "Compute the package difference that will be applied to the system on a switch";
-            command = ''
-              os-build
-              ${pkgs.nvd}/bin/nvd diff /run/current-system ./result
-            '';
-          }
-          {
-            name = "os-build";
-            category = "NixOS";
-            help = "Build NixOS configuration without applying";
-            command = ''
-              nixos-rebuild build --flake '.#notebook' --impure $@
-            '';
-          }
-          {
-            name = "os-boot";
-            category = "NixOS";
-            help = "Apply NixOS configuration as the default boot profile, but don't load it immediately";
-            command = ''
-              sudo nixos-rebuild boot --flake '.#notebook' --impure $@
-            '';
-          }
-          {
-            name = "os-purge";
-            category = "NixOS";
-            help = "Drop a system profile and purges it from the bootloader";
-            command = ''
-              sudo rm -vrf /nix/var/nix/profiles/system-profiles/$1 /nix/var/nix/profiles/system-profiles/$1-*-link
-              sudo /nix/var/nix/profiles/system/bin/switch-to-configuration switch
-            '';
-          }
-
-          # Utility
-          {
-            name = "clear";
-            category = "Utility";
-            help = "Clear build result";
-            command = ''
-              rm -rf ./result
-            '';
-          }
-          {
-            name = "fmt";
-            category = "Utility";
-            help = "Format nix files";
-            command = ''
-              ${pkgs.statix}/bin/statix fix
-              nix fmt $@
-            '';
-          }
-          {
-            name = "gc";
-            category = "Utility";
-            help = "Garbage collection";
-            command = ''
-              sudo nix-collect-garbage $@
-            '';
-          }
-          {
-            name = "gc-all-gen";
-            category = "Utility";
-            help = ''Delete old generations and garbage collection'';
-            command = ''
-              sudo nix-collect-garbage -d $@
-            '';
-          }
-        ];
+        commands = builtins.map
+          (cmd: cmd // {
+            command = self.outputs.apps.${system}.${cmd.name}.program;
+          })
+          commands;
       };
     });
 }
