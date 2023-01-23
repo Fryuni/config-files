b44f30895d976a0e2b1265c42abf8d98bb89e066
Luiz Ferraz <luiz@lferraz.com>
2023-01-23 10:25:00 -0300

    feat(os): Disable ideapad_laptot kernel module

--- nixos/notebook/default.nix
+++ nixos/notebook/default.nix
@@ -13,6 +13,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
+  boot.blacklistedKernelModules = ["ideapad_laptop"];
   boot.extraModulePackages = with config.boot.kernelPackages; [zenpower];
 
   networking.hostName = "lotus-notebook";
