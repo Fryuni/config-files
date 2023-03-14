058dc22483adabd8693fcbd534efcdc4703ccc7b
Luiz Ferraz <luiz@lferraz.com>
2023-03-14 16:56:09 -0300

    chore: Format Zellij starter file

--- nix-home/terminal/zellijStarter.nix
+++ nix-home/terminal/zellijStarter.nix
@@ -2,53 +2,52 @@
   rustCrates,
   gum,
   writeShellScriptBin,
-}:
-let
+}: let
   inherit (rustCrates) zellij;
 in
-writeShellScriptBin "__start_zellij" ''
-  trim() {
-    local var="$*"
-    # remove leading whitespace characters
-    var="''${var#"''${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
-    var="''${var%"''${var##*[![:space:]]}"}"
-    printf '%s' "$var"
-  }
+  writeShellScriptBin "__start_zellij" ''
+    trim() {
+      local var="$*"
+      # remove leading whitespace characters
+      var="''${var#"''${var%%[![:space:]]*}"}"
+      # remove trailing whitespace characters
+      var="''${var%"''${var##*[![:space:]]}"}"
+      printf '%s' "$var"
+    }
 
-  start_zellij() {
-    local session_name="$1"
+    start_zellij() {
+      local session_name="$1"
 
-    if [ -z "$session_name"]; then
-      ${gum}/bin/gum style --faint --italic --bold "Zellij session name:"
+      if [ -z "$session_name"]; then
+        ${gum}/bin/gum style --faint --italic --bold "Zellij session name:"
 
-      session_name=$(${gum}/bin/gum input --char-limit=40)
-    fi
+        session_name=$(${gum}/bin/gum input --char-limit=40)
+      fi
 
-    if [ -z "$session_name" ]; then
-      exec ${zellij}/bin/zellij
-    else
-      exec ${zellij}/bin/zellij -s "$session_name"
-    fi
-  }
-
-  if [ -z "$ZELLIJ" ]; then
-    ongoing_sessions=$(trim "$(${zellij}/bin/zellij ls 2>/dev/null)")
-
-    if [ -z "$ongoing_sessions" ]; then
-      start_zellij main
-    else
-      ${gum}/bin/gum style --faint --italic --bold "Chose a session (or create a new one):"
-
-      pick_options=$(trim "''${ongoing_sessions}\nCreate new session")
-      chosen=$(echo -e "$pick_options" | ${gum}/bin/gum filter)
-
-      if [ "$chosen" = "Create new session" ]; then
-        ${gum}/bin/gum style --faint --italic -- "--> $chosen"
-        start_zellij
+      if [ -z "$session_name" ]; then
+        exec ${zellij}/bin/zellij
       else
-        exec ${zellij}/bin/zellij attach "$chosen"
+        exec ${zellij}/bin/zellij -s "$session_name"
+      fi
+    }
+
+    if [ -z "$ZELLIJ" ]; then
+      ongoing_sessions=$(trim "$(${zellij}/bin/zellij ls 2>/dev/null)")
+
+      if [ -z "$ongoing_sessions" ]; then
+        start_zellij main
+      else
+        ${gum}/bin/gum style --faint --italic --bold "Chose a session (or create a new one):"
+
+        pick_options=$(trim "''${ongoing_sessions}\nCreate new session")
+        chosen=$(echo -e "$pick_options" | ${gum}/bin/gum filter)
+
+        if [ "$chosen" = "Create new session" ]; then
+          ${gum}/bin/gum style --faint --italic -- "--> $chosen"
+          start_zellij
+        else
+          exec ${zellij}/bin/zellij attach "$chosen"
+        fi
       fi
     fi
-  fi
-''
+  ''
