02a9d9d7df7a8007da78f9c32f34bfa1b04c1dec
Luiz Ferraz <luiz@lferraz.com>
2023-06-21 19:31:23 -0300

    chore: Update JetBrains IDEs

--- overlay/jetbrains.nix
+++ overlay/jetbrains.nix
@@ -4,21 +4,21 @@ final: pkgs: let
   overrides = {
     # https://www.jetbrains.com/webstorm/download/other.html
     webstorm = rec {
-      version = "2023.1.2";
+      version = "2023.1.3";
       url = "https://download.jetbrains.com/webstorm/WebStorm-${version}.tar.gz";
-      sha256 = "sha256-k0mG1oKFfoUpWIzcb08SX/fhPuChBg+kGvK7nUpiBEQ=";
+      sha256 = "sha256-HO8YptgOBjtSDdjpoM9bJ6nLBbv6W2gOl8VKfLQ1ycY=";
     };
     # https://www.jetbrains.com/go/download/other.html
     goland = rec {
-      version = "2023.1.2";
+      version = "2023.1.3";
       url = "https://download.jetbrains.com/go/goland-${version}.tar.gz";
-      sha256 = "sha256-4fFnJqhk9P+fCki9YKaYOmZAMN9eVFYCPXa4+4rJ350=";
+      sha256 = "sha256-9FYCThtd5AWtqb1XBIP/yhEm7eycgb+gKwd/gUNLVxk=";
     };
     # https://www.jetbrains.com/pycharm/download/other.html
     pycharm-professional = rec {
-      version = "2023.1.2";
+      version = "2023.1.3";
       url = "https://download.jetbrains.com/python/pycharm-professional-${version}.tar.gz";
-      sha256 = "sha256-5X6uejyZmDuNxcWqA2V516xzyuM660xff4BRfyBAw4U=";
+      sha256 = "sha256-TN+FwBhU1/dMn6nv2mdFM1bxEg5JzFrtEWjw8y2O4BY=";
     };
   };
 
