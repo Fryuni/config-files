7288638cffc97ffc2f9351eb2f68ab5703715b22
Luiz Ferraz <luiz@lferraz.com>
2023-06-21 03:12:00 -0300

    chore: Add nix-shell Cachix entry

--- nixos/nix-settings.nix
+++ nixos/nix-settings.nix
@@ -23,12 +23,14 @@
     auto-optimise-store = true;
 
     substituters = [
+      "https://nix-shell.cachix.org"
       "https://nix-gaming.cachix.org"
       "https://nix-community.cachix.org"
       "https://nixpkgs.cachix.org"
       "https://fryuni.cachix.org"
     ];
     trusted-public-keys = [
+      "nix-shell.cachix.org-1:kat3KoRVbilxA6TkXEtTN9IfD4JhsQp1TPUHg652Mwc="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs.cachix.org-1:q91R6hxbwFvDqTSDKwDAV4T5PxqXGxswD8vhONFMeOE="
