386220285ff3e799d55ec1815b28255b83b415d5
Luiz Ferraz <luiz@lferraz.com>
2023-08-28 20:46:09 -0300

    chore(flake): Workaround for inactionable warning on system build
    - Workaround for https://github.com/NixOS/nixpkgs/issues/219567

--- overlay/default.nix
+++ overlay/default.nix
@@ -74,4 +74,13 @@ final: pkgs:
 
     vendorSha256 = "sha256-f82ibPnauUOuZ5D6Rz3Yyt0jiAXvjN8Or3gud+ri6FA=";
   };
+
+  # Workaround for https://github.com/NixOS/nixpkgs/issues/219567
+  xorg = pkgs.xorg // {
+    xrandr = pkgs.xorg.xrandr.overrideAttrs (_: prev: {
+      meta = prev.meta // {
+        mainProgram = "xrandr";
+      };
+    });
+  };
 }
