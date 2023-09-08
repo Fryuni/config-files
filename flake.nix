0a9ae6cf6ea5ed4f1ce270f24753c995e39fdc2c
Luiz Ferraz <luiz@lferraz.com>
2023-09-08 19:09:37 -0300

    chore(flake): Refactor overlays

--- flake.nix
+++ flake.nix
@@ -71,76 +71,17 @@
         inherit system;
         config.allowUnfree = true;
         config.permittedInsecurePackages = [];
-        overlays = [
-          (pkgs: prev: {
-            inherit stable master;
-
-            croct-php = prev.php81.withExtensions ({
-              enabled,
-              all,
-            }:
-              with all;
-                enabled
-                ++ [
-                  mbstring
-                  filter
-
-                  openssl
-                  curl
-                  pdo
-                  pdo_pgsql
-                  pdo_sqlite
-                  redis
-                  bcmath
-                  intl
-                  swoole
-                  dom
-                  simplexml
-                  xmlwriter
-                  tokenizer
-
-                  xdebug
-                  pcov
-                  # (pkgs.php81.buildPecl rec {
-                  #   pname = "decimal";
-                  #   version = "v1.4.0";
-                  #
-                  #   LIBMPDEC_DIR = "${pkgs.mpdecimal.dev}/include";
-                  #
-                  #   src = pkgs.fetchFromGitHub {
-                  #     owner = "php-decimal";
-                  #     repo = "ext-decimal";
-                  #     rev = version;
-                  #     sha256 = "sha256-1xP6DqRWK5fFaPNwXXzTjZVkFzxmG9Uzy8qxrYjYvbA=";
-                  #   };
-                  # })
-                ]);
-
-            croct-php-env = prev.stdenvNoLibs.mkDerivation {
-              pname = "croct-php-env";
-              version = "8.1";
-              # buildInputs = [
-              #   croct-php-pkg
-              #   croct-php-pkg.packages.composer
-              # ];
-
-              dontUnpack = true;
-              dontBuild = true;
-
-              NIX_DEBUG = 10;
-              installPhase = ''
-                mkdir -p $out/bin
-                ln -s "${pkgs.croct-php}/bin/php" "$out/bin/php"
-                ln -s "${pkgs.croct-php.packages.composer}/bin/composer" $out/bin/composer
-              '';
-            };
-          })
-          fenix.overlays.default
-          agenix.overlays.default
-          (import ./overlay)
-          attrs.polymc.overlay
-          # devshell.overlay
-        ];
+        overlays =
+          [
+            (_: _: {inherit master stable;})
+            fenix.overlays.default
+            agenix.overlays.default
+          ]
+          ++ (import ./overlay)
+          ++ [
+            attrs.polymc.overlay
+            # devshell.overlay
+          ];
       };
 
     globalConfig = {
@@ -183,8 +124,7 @@
       homeManager = "${home-manager.packages.${system}.home-manager}/bin/home-manager";
       commands = let
         bins = builtins.mapAttrs (name: pkg: pkgs.lib.meta.getExe pkg) pkgs;
-      in
-      [
+      in [
         # Setup
         {
           name = "activate/notebook";
--- overlay/rustPackages/default.nix
+++ overlay/rustPackages/default.nix
@@ -1,31 +1,31 @@
-pkgs: let
-  inherit (pkgs.fenix.complete) toolchain;
-  rustPlatform = pkgs.makeRustPlatform {
+final: prev: let
+  inherit (final.fenix.complete) toolchain;
+  rustPlatform = final.makeRustPlatform {
     cargo = toolchain;
     rustc = toolchain;
   };
 
   generated = import ./data.nix;
-  extras = import ./extras.nix pkgs;
+  extras = import ./extras.nix final;
 
   makePkg = name: definition: let
     extra = extras.${name} or {};
     safeExtras = builtins.removeAttrs extra ["nativeBuildInputs"];
   in
-    pkgs.rustPlatform.buildRustPackage ({
+    final.rustPlatform.buildRustPackage ({
         pname = name;
         inherit (definition) version;
 
         doCheck = false;
 
-        nativeBuildInputs = with pkgs;
+        nativeBuildInputs = with final;
           (extra.nativeBuildInputs or [])
           ++ [
             openssl.dev
             pkg-config
           ];
 
-        src = pkgs.fetchCrate {
+        src = final.fetchCrate {
           pname = name;
           inherit (definition) version;
           sha256 = definition.crateSha256;
@@ -37,7 +37,7 @@ pkgs: let
           homepage = definition.homepage or "https://crates.io/crates/${name}";
           documentation = "https://docs.rs/${name}/${definition.version}";
           mainProgram = definition.mainProgram or name;
-          maintainers = [pkgs.lib.maintainers.fryuni];
+          maintainers = [final.lib.maintainers.fryuni];
         };
       }
       // safeExtras);
