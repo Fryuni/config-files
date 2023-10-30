4a0c3dafbc78206d46f8f8dd01b7da8c4b954415
Luiz Ferraz <luiz@lferraz.com>
2023-10-30 15:27:38 -0300

    feat: Configure global prr

--- nix-home/secrets.nix
+++ nix-home/secrets.nix
@@ -1,7 +1,12 @@
-_: {
+{config, ...}: {
   age.secrets = {
     github-key.file = ../secrets/github-key;
     habitica-key.file = ../secrets/habitica-key;
     openai-key.file = ../secrets/openai-key;
+
+    prr-global = {
+      file = ../secrets/prr-global;
+      path = "${config.xdg.configHome}/prr/config.toml";
+    };
   };
 }
