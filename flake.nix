403e51886f7e59800187cfda773ae30d52921f7b
Luiz Ferraz <luiz@lferraz.com>
2023-12-11 13:18:22 -0300

    chore(nix): Allow EOL Electron v25

--- flake.nix
+++ flake.nix
@@ -45,11 +45,17 @@
     agenix,
     ...
   } @ attrs: let
+    pkgsCfg = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "electron-25.9.0"
+      ];
+    };
+
     subPkgs = system: flake:
       import flake {
         inherit system;
-        config.allowUnfree = true;
-        config.permittedInsecurePackages = [];
+        config = pkgsCfg;
       };
 
     pkgsFun = system: let
@@ -59,14 +65,13 @@
       import nixpkgs {
         inherit system;
 
-        config = {
-          allowUnfree = true;
-          permittedInsecurePackages = [];
-
-          # https://github.com/NixOS/nixpkgs/pull/258447
-          # https://discourse.nixos.org/t/your-system-configures-nixpkgs-with-an-externally-created-instance/33802
-          pulseaudio = true;
-        };
+        config =
+          pkgsCfg
+          // {
+            # https://github.com/NixOS/nixpkgs/pull/258447
+            # https://discourse.nixos.org/t/your-system-configures-nixpkgs-with-an-externally-created-instance/33802
+            pulseaudio = true;
+          };
 
         overlays =
           [
