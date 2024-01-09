b8cd62d8acab8540116d7e601e56229400151668
Luiz Ferraz <luiz@lferraz.com>
2024-01-09 09:40:18 -0300

    chore(term): Migrate from deprecated Alacritty option

--- nix-home/terminal/alacritty.nix
+++ nix-home/terminal/alacritty.nix
@@ -18,7 +18,9 @@
       startup_mode = "Maximized";
     };
 
-    draw_bold_text_with_bright_colors = true;
+    colors = {
+      draw_bold_text_with_bright_colors = true;
+    };
 
     font = let
       family = "JetBrainsMono Nerd Font";
