2253aac38ac95cb961f7b62fb84a2647c36c00d8
Luiz Ferraz <luiz@lferraz.com>
2024-01-21 15:27:22 -0300

    feat(tools): Add npmrc

--- nix-home/secrets.nix
+++ nix-home/secrets.nix
@@ -8,5 +8,9 @@
       file = ../secrets/prr-global;
       path = "${config.xdg.configHome}/prr/config.toml";
     };
+    npm-token = {
+      file = ../secrets/npm-token;
+      path = "/home/lotus/.npmrc";
+    };
   };
 }
