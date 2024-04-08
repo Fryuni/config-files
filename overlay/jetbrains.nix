d52456ec34e01a31daf31034143d4cb35971e674
Luiz Ferraz <luiz@lferraz.com>
2024-04-08 18:30:29 -0300

    feat(ide): Add Codeium

--- overlay/jetbrains.nix
+++ overlay/jetbrains.nix
@@ -5,6 +5,7 @@ final: pkgs: let
   globalPlugins = [
     "164" # IdeaVIM
     "17718" # GitHub Copilot
+    "20540" # Codeium
     "9836" # Randomness
     "7425" # Wakatime
     "7499" # Git Toolbox
