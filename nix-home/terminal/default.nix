d110c2264fa74b1d7083bf4464b72f6dc79806ed
Luiz Ferraz <luiz@lferraz.com>
2024-04-26 08:22:48 -0300

    chore: Don't start Zellij automatically

--- nix-home/terminal/default.nix
+++ nix-home/terminal/default.nix
@@ -48,11 +48,10 @@
 
     initExtraFirst = ''
       setopt cdable_vars
-
-      if [ -z "$ZELLIJ" ] && [ -z "$TERMINAL_EMULATOR" ]; then
-        exec zellij
-      fi
     '';
+    # if [ -z "$ZELLIJ" ] && [ -z "$TERMINAL_EMULATOR" ]; then
+    #   exec zellij
+    # fi
 
     dirHashes = {
       nix-system = "/run/current-system";
