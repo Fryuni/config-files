cab4b19887bcd3830836c9f151ea8a89e6bd1ae8
Luiz Ferraz <luiz@lferraz.com>
2024-06-11 23:58:24 -0300

    chore(ides): Disable unnused IDEs

--- nix-home/development/ides.nix
+++ nix-home/development/ides.nix
@@ -2,10 +2,10 @@
   home.packages = with pkgs; [
     jetbrains.pycharm-professional
     jetbrains.goland
-    jetbrains.rust-rover
+    # jetbrains.rust-rover
     jetbrains.webstorm
     jetbrains.datagrip
-    jetbrains.idea-ultimate
+    # jetbrains.idea-ultimate
     # android-studio
 
     insomnia
