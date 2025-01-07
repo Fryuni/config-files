eb14242b535a5d313d200e9adba0d325d195c893
Luiz Ferraz <luiz@lferraz.com>
2025-01-07 16:23:15 -0300

    --wip-- [skip ci]

--- servers/gce-automation/default.nix
+++ servers/gce-automation/default.nix
@@ -3,6 +3,12 @@
     ../common-gce.nix
   ];
 
+  fileSystems."/data" = {
+    fsType = "ext4";
+    device = "/dev/disk/by-label/data";
+    autoResize = true;
+  };
+
   environment.systemPackages = with pkgs; [
     twitch-dl
     twitch-cli
