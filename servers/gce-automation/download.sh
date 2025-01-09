989f240f2173288edda26c901807c5a2fa3fec55
Luiz Ferraz <luiz@lferraz.com>
2025-01-08 21:42:33 -0300

    --wip-- [skip ci]

--- servers/gce-automation/download.sh
+++ servers/gce-automation/download.sh
@@ -28,7 +28,7 @@ for channel in "${CHANNELS[@]}"; do
     bash ./download-and-store.sh "$video_id"
   done
 
-  export OUT_NAME_FORMAT="$channel/vods/{date}_{id}_{channel_login}_{title_slug}.{format}"
+  export OUT_NAME_FORMAT="$channel/clips/{date}_{id}_{channel_login}_{title_slug}.{format}"
 
   twitch-dl clips "$channel" --limit 50 --json |
     jq '.[].slug' -r |
