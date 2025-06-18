db8283b62b5707038aa82d6f33f0aff2d8a42161
Luiz Ferraz <luiz@lferraz.com>
2025-06-17 21:34:55 -0300

    chore: Update state version

--- nixos/default.nix
+++ nixos/default.nix
@@ -84,6 +84,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05";
+  system.stateVersion = "24.11";
   system.autoUpgrade.enable = true;
 }
