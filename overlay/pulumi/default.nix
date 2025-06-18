2d6d7ba156035e5f7217117e3e2bf58eed27f49c
Luiz Ferraz <luiz@lferraz.com>
2025-06-17 21:53:47 -0300

    chore: Flag Pulumi's non-standard  version override

--- overlay/pulumi/default.nix
+++ overlay/pulumi/default.nix
@@ -3,6 +3,7 @@ final: pkgs: let
 in {
   pulumi-bin = pkgs.pulumi-bin.overrideAttrs (_: _: {
     inherit (data) version;
+    __intentionallyOverridingVersion = true;
 
     srcs = map pkgs.fetchurl data.pulumiPkgs.${pkgs.system};
   });
