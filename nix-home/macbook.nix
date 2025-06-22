3fed662eca25b1b5f4efe79618fa5a6506a7a14d
Luiz Ferraz <luiz@lferraz.com>
2025-06-22 18:13:45 -0300

    feat(darwin): Configure direnv for Mac

--- nix-home/macbook.nix
+++ nix-home/macbook.nix
@@ -14,4 +14,16 @@
       keyid-format = "short";
     };
   };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+
+    config = {
+      global = {
+        load_dotenv = true;
+      };
+    };
+  };
 }
