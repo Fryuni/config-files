1fe6ac6dca68803dae00f2a42ff11ca18c081eab
Luiz Ferraz <luiz@lferraz.com>
2025-10-28 12:13:35 -0300

    chore(cli): Update pulumi

--- overlay/pulumi/data.nix
+++ overlay/pulumi/data.nix
@@ -1,11 +1,11 @@
 # DO NOT EDIT! This file is generated automatically by update.sh
 _: {
-  version = "3.203.0";
+  version = "3.204.0";
   pulumiPkgs = {
     x86_64-linux = [
       {
-        url = "https://get.pulumi.com/releases/sdk/pulumi-v3.203.0-linux-x64.tar.gz";
-        sha256 = "0jb01rr4a1fxgdcbbk5rdkbya7fkqlqj6pyklji4xv8r9pn21fyk";
+        url = "https://get.pulumi.com/releases/sdk/pulumi-v3.204.0-linux-x64.tar.gz";
+        sha256 = "0g50a7571gvg9idcbxm26msibl9blb920p6r9lzyh51mmsfsf5hp";
       }
       {
         url = "https://api.pulumi.com/releases/plugins/pulumi-resource-docker-v4.9.0-linux-amd64.tar.gz";
@@ -16,8 +16,8 @@ _: {
         sha256 = "04m8b9d1dsfm3kw3n214g46i946fgn56j0k10dzfa2l023y491gc";
       }
       {
-        url = "https://api.pulumi.com/releases/plugins/pulumi-resource-github-v6.7.4-linux-amd64.tar.gz";
-        sha256 = "0yyx1rgw43fyxc2560w61dkps20bbrxkpiq2cnis5ibg0aj988za";
+        url = "https://api.pulumi.com/releases/plugins/pulumi-resource-github-v6.8.0-linux-amd64.tar.gz";
+        sha256 = "0f07prmwm0rgd5r2amd3fxm9cpnfrgj2282d6iczl7pnnl5jn7wp";
       }
       {
         url = "https://api.pulumi.com/releases/plugins/pulumi-resource-gitlab-v9.4.0-linux-amd64.tar.gz";
@@ -44,8 +44,8 @@ _: {
         sha256 = "1vh28r2gk85a82c7jsa95n21my19hzavkyc9pjdpw517r63gvyjw";
       }
       {
-        url = "https://api.pulumi.com/releases/plugins/pulumi-resource-vault-v7.3.0-linux-amd64.tar.gz";
-        sha256 = "0vdrh5ss7vlmyv0a0mvrj2w9mrsazqrl7hcvwn5riym4ba0daq03";
+        url = "https://api.pulumi.com/releases/plugins/pulumi-resource-vault-v7.3.1-linux-amd64.tar.gz";
+        sha256 = "168a1hqpildnal3xnivpimlmfr9dgkcrc1fp03www96qvnwa5b0c";
       }
     ];
   };
