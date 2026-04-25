4a48f3587a5ef838b2d2c92456aa582237481f21
Luiz Ferraz <luiz@lferraz.com>
2026-04-25 20:31:49 -0300

    chore(nix): Clean up some configuration

--- servers/loem/default.nix
+++ servers/loem/default.nix
@@ -24,16 +24,8 @@
     devices = ["/dev/sda" "/dev/sdb"];
   };
 
-  # boot.initrd.availableKernelModules = [
-  #   "xhci_pci"
-  #   "ahci"
-  #   "sd_mod"
-  # ];
-
   boot.swraid.enable = true;
 
-  # Verify interface name and gateway in Hetzner Robot panel before deploying.
-  # Run `ip link` in the rescue system to confirm the interface name.
   networking.useDHCP = false;
   networking.interfaces."enp0s31f6" = {
     ipv4.addresses = [
@@ -65,8 +57,7 @@
   };
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI4D6ddYz7WosKUA4Xr7R1cwLF/mpCSWrCSW3O9Ct7E luiz@lferraz.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYY0uHuJGkwcZOsZLqUgdNw6FMxYkz5pY0YeUgmr8dw luiz@lferraz.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBYY0uHuJGkwcZOsZLqUgdNw6FMxYkz5pY0YeUgmr8dw"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICphbHvsvJiWjPAV8+JlUZfMHZtXIcp9L+cxn6Y9pjBZ"
   ];
 }
