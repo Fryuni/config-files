d3f56efed1704bb51503d035c77b0cf89b84baf2
Luiz Ferraz <luiz@lferraz.com>
2026-06-03 01:04:55 +0000

    feat(servers): added base system packages to remote dev servers
    - Changed module argument from inputs to pkgs to support package declarations.
    - Added curl, wget, git, dig, htop, btop, gnumake, and agenix to remoteDev configuration.

--- servers/remoteDev.nix
+++ servers/remoteDev.nix
@@ -1,6 +1,17 @@
-{inputs, ...}: {
+{pkgs, ...}: {
   imports = [
     ./interactive.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+    curl
+    wget
+    git
+    dig
+    htop
+    btop
+    gnumake
+    agenix
   ];
 
   home-manager = {
