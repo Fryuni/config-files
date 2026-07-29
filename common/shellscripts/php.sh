236ee14af4525cffca60133aa5da69afb64c4693
Luiz Ferraz <luiz@lferraz.com>
2026-07-28 23:55:21 -0300

    fix(shell): Check for tty on stdin before running PHP image interactively

--- common/shellscripts/php.sh
+++ common/shellscripts/php.sh
@@ -4,11 +4,18 @@ set -eo pipefail
 
 SHIM_NAME="$(basename "$0" .sh)"
 
-exec docker run -it --rm \
-  -u "$UID:$GID" \
-  -v "/run/user/$UID:/run/user/$UID" \
-  -v /home/lotus:/home/lotus \
-  -w "$(pwd)" \
-  -e HOME="$HOME" \
-  crocttech/php-base-image:php8.5-dev \
-  "$SHIM_NAME" "$@"
+args=(
+  --rm
+  -u "$UID:$GID"
+  -v "/run/user/$UID:/run/user/$UID"
+  -v /home/lotus:/home/lotus
+  -w "$(pwd)"
+  -e HOME="$HOME"
+)
+
+if [ -t 0 ]; then
+  # Stdin is a terminal, bind it to docker
+  args+=("-it")
+fi
+
+exec docker run "${args[@]}" crocttech/php-base-image:php8.5-dev "$SHIM_NAME" "$@"
