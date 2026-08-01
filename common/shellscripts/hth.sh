ac7093e84c97fb9f0b98c099d724d52326331b62
Luiz Ferraz <luiz@lferraz.com>
2026-08-01 13:01:49 -0300

    fix(sh): Correct herdr worktree creation with treehouse

--- common/shellscripts/hth.sh
+++ common/shellscripts/hth.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+WID="$(openssl rand -hex 1)"
+
 WT_PATH="$(treehouse get --lease)"
 
-herdr worktree open "$WT_PATH"
-
 treehouse return "$WT_PATH"
+
+cd "$WT_PATH" || return
+
+if [ -z "$1" ]; then
+  git switch -C "wt/$WID"
+  WID="main/$WID"
+else
+  if git switch "$1"; then
+    WID="$1"
+  else
+    git switch -C "wt/$1/$WID" "$1"
+    WID="$1/$WID"
+  fi
+fi
+
+herdr worktree open --path "$WT_PATH" --label "$WID" --focus
--- nix-home/terminal/default.nix
+++ nix-home/terminal/default.nix
@@ -8,6 +8,7 @@
 
   home.packages = with pkgs; [
     pfetch
+    openssl
   ];
 
   programs.starship = {
