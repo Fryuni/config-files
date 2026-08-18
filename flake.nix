f3f181ad4a3d24e2b5748b3c0a10e76e04fc0c8e
Luiz Ferraz <luiz@lferraz.com>
2026-08-18 14:57:41 -0300

    chore: Use canary Bun for LLM agents

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
     };
     llm-agents = {
       # url = "github:numtide/llm-agents.nix";
-      url = "git+https://codeberg.org/Fryuni/llm-agents.nix?ref=main";
+      url = "git+https://git.fryuni.dev/Fryuni/llm-agents.nix?ref=bun-canary";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
     nur = {
