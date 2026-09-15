fefa3960b1fcf944f1189538d9bd773bc073d114
Luiz Ferraz <luiz@lferraz.com>
2026-09-15 01:37:57 +0000

    chore: Disable GitHub runners

--- servers/loem/github-actions-runners.nix
+++ servers/loem/github-actions-runners.nix
@@ -3,6 +3,6 @@
   # Personal repositories need one entry each; organizations need only one.
   # Reuse a credential name for repositories covered by the same PAT.
   # Credentials live in secrets/loem/github-actions-<credential>-token.
-  "Fryuni/t3code" = "fryuni";
-  "fryuni-testorg" = "fryuni-testorg";
+  # "Fryuni/t3code" = "fryuni";
+  # "fryuni-testorg" = "fryuni-testorg";
 }
